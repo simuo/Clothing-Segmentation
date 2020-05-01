f08045793eb7d0ecc341a554cbd743dc4b39eecd
simuo <3286649448@qq.com>
2020-05-01 11:23:13 +0800

    Update train.py

--- src/train.py
+++ src/train.py
@@ -9,18 +9,19 @@ from Unet import Unet
 if __name__ == '__main__':
     writer=SummaryWriter('/content/Clothing-Segmentation/src/runs')
     dataset = MydataSet()
-    dataloader = DataLoader(dataset, batch_size=5, shuffle=True, num_workers=8)
+    dataloader = DataLoader(dataset, batch_size=1, shuffle=True, num_workers=8)
     module = Unet(3, 59).cuda()
     if os.path.exists(r'/content/Clothing-Segmentation/src/params/params2.pt'):
       module.load_state_dict(torch.load('/content/Clothing-Segmentation/src/params/params2.pt'))
     optimizer = optim.Adam(module.parameters())
     criticizer = nn.CrossEntropyLoss()
-    for epoch in range(1000):
+    for epoch in range(10000):
       for i, (data, lable) in enumerate(dataloader):
         data = data.cuda()
         lable = lable.cuda()
         output = module(data)
-        loss1 = criticizer(output, lable.squeeze(1).long())
+        loss1 = criticizer(output, lable.squeeze(0).long())
+        print(loss1)
         writer.add_scalar('Loss/train', loss1.item(), i)
         # loss2 = 1 - get_DC(output.cpu(),lable.cpu())
         # print(loss1.item(), loss2.item())
@@ -30,5 +31,5 @@ if __name__ == '__main__':
         loss1.backward()
         optimizer.step()
         print(f'EPOCH {epoch}, {i} / {len(dataloader)} , loss:{loss1.item()}')
-      torch.save(module.state_dict(), r'/content/Clothing-Segmentation/src/params/params{}.pt'.format(epoch+3))
+      torch.save(module.state_dict(), r'/content/Clothing-Segmentation/src/params/params{}.pt'.format(epoch))
     writer.close()
